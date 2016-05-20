a74d82a1a0c2c20b68ae15b7a29953e9f39ec151
Chloe Fitton <chloe.lea.fitton@mail.com>
2016-05-20 12:45:54 +1000

    add DELETE todos route

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require ('express');
 var bodyParser = require ('body-parser');
 var _ = require('underscore');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
@@ -39,6 +40,19 @@ app.post('/todos', function (req, res) {
 	body.id = todoNextId++;
 	todos.push(body);
 	res.json(body);
+});
+
+
+app.delete('/todos/:id', function (req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	
+	if (!matchedTodo) {
+		res.status(404).json({"error": "No todo found with that id"});
+	} else {
+		todos = _.without(todos, matchedTodo);
+		res.json(matchedTodo);
+	}
 });
 
 
